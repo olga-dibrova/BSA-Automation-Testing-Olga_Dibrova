82a2e69d5e4bb477c37d6f190d73c3aceddf80bd
olga-dibrova <83549966+olga-dibrova@users.noreply.github.com>
2021-07-21 12:18:36 +0300

    add tests 1-2

--- specs/wdio.spec.js
+++ specs/wdio.spec.js
@@ -4,7 +4,7 @@ const rundomNumber = () => Date.now();
 
 const app = new App();
 
-describe('Registration:', function () {
+describe('Login:', function () {
   beforeEach(async function () {
     await browser.setWindowSize(1440, 960);
     await browser.url('/sign-up');
@@ -14,27 +14,48 @@ describe('Registration:', function () {
     await browser.reloadSession();
   });
 
-  it('should be able to register doctor', async function () {
-    await app.authPage.register({
-      name: `John${rundomNumber()}`,
-      surname: 'Doctor',
-      email: `marcus${rundomNumber()}@gmail.com`,
-      password: 'Pa55word',
-      phone: '380999999',
-      birthDate: '11/11/2000',
-      status: 'doctor',
-      gender: 'female',
+    it('should be able to login', async function () {
+
+     await app.authPage.login({
+       email:'john_admin1@admin.com',
+       password:'Pa55word'
+      })
+  
+      await browser.waitUntil(
+        async function () {
+          const url = await browser.getUrl();
+          return url === 'http://46.101.234.121/doctors';
+        },
+        { timeout: 5000 },
+      );
+  
+      const url = await browser.getUrl();
+      expect(url).to.be.eql('http://46.101.234.121/doctors');
+      
+     
     });
+ 
 
-    await browser.waitUntil(
-      async function () {
-        const url = await browser.getUrl();
-        return url === 'http://46.101.234.121/user-profile/aa5058a3-3e09-4db4-b8fb-2232cc612265';
-      },
-      { timeout: 5000 },
-    );
+// Login invalid data
 
-    const url = await browser.getUrl();
-    expect(url).to.be.eql('http://46.101.234.121/doctors');
+    it('should not be able to login', async function () {
+  
+      await app.authPage.login({
+        email:'john1@admin.com',
+        password:'Pa22word'
+       })
+  
+      await browser.waitUntil(
+        async function () {
+          const url = await browser.getUrl();
+          return url === 'http://46.101.234.121/sign-in';
+        },
+        { timeout: 5000 },
+      );
+  
+      const url = await browser.getUrl();
+      expect(url).to.be.eql('http://46.101.234.121/sign-in');
+      
+    });
+  
   });
-});
